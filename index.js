a15960ce6da260be45d9fafec7abdd44b9b4abed
Will Munn <will.munn@focusrite.com>
2023-03-28 06:26:16 +0100

    Without post js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const mod = require('./cmake-build/wasmfun');
+mod.onRuntimeInitialized = () => {
+  const newFuncPtr = mod.addFunction(function (num) {
+    console.log('wooop');
+    //Module.print('Hello ' + num + ' from JS!');
+  }, 'vi');
+  mod._callMeBack(newFuncPtr);
+}
--- post.js
+++
@@ -1,9 +0,0 @@
-Module.onRuntimeInitialized = () => {
-  const newFuncPtr = Module.addFunction(function(num) {
-    console.log('wooop');
-    //Module.print('Hello ' + num + ' from JS!');
-  }, 'vi');
-  Module._callMeBack(newFuncPtr);
-
-  console.log(Module._add3(5));
-};
