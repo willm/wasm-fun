9704c380fbc6d78683327d4453c9802a3a6917f5
Will Munn <will.munn@focusrite.com>
2023-03-28 06:34:19 +0100

    Remove unused main method and call add3 function in html example

--- main.cpp
+++ main.cpp
@@ -20,13 +20,3 @@ void callMeBack(int callbackPointer) {
     callback(2);
     EM_ASM_(Module.removeFunction($0), callback);
 }
-
-int main(int argc, char **argv) {
-    int fp = atoi(argv[1]);
-    printf("fp: %d\n", fp);
-    void (*f)(int) = reinterpret_cast<void (*)(int)>(fp);
-    f(7);
-    EM_ASM_(Module.Runtime.removeFunction($0), f);
-    printf("ok\n");
-    return 0;
-}
